3242e7558480743d87d70e8c8262c5aa867b00b6
yohanym95 <malshikay@gmail.com>
2020-05-19 21:09:16 +0530

    Updates

---
+++ precache-manifest.87d5d3f149e1c174c0660b85b076549d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0b11204cb30c10df788",
+    "url": "/gh-pages-react-app/static/css/main.a68a5783.chunk.css"
+  },
+  {
+    "revision": "a0b11204cb30c10df788",
+    "url": "/gh-pages-react-app/static/js/main.e15df653.chunk.js"
+  },
+  {
+    "revision": "a585ebbc004a99d66cd4",
+    "url": "/gh-pages-react-app/static/js/runtime~main.84b94a0a.js"
+  },
+  {
+    "revision": "abc921c608ebbf2c207b",
+    "url": "/gh-pages-react-app/static/js/2.fdc7f0fe.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "29bf3b30f04ae709eb094874a1cb8807",
+    "url": "/gh-pages-react-app/index.html"
+  }
+];
